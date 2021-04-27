7d0e5b71bfca7fe6cb995cfd224a908a4b250f60
davisstacked <brettzvi@gmail.com>
2021-04-27 15:33:27 -0700

    fixed responsive design on home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,20 +7,20 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home">
-      <div className="unicorn-container">
+      {/* <div className="unicorn-container"> */}
         <h1>Woah! I am a Unicorn!  <i class="far fa-laugh-beam"></i>
         </h1>
         <UnicornImage />
         <p>No, I'm not a privately held startup company valued at over $1 billion</p>
         <p>I'm the real deal, a mystical creature of magic.</p>
         <p>I'm being hunted by a Monster. <i class="far fa-sad-cry fa-lg"></i></p>
-        <p>Can I trust you to Pilot me safely to the <span>rainbow</span>?</p>
+        <p>Can I Trust you to Pilot me safely to the <span>RAINBOW?</span></p>
         <Link to="/game">
           <button className="start-game-button">
             Yes! Let's go! 
           </button>
         </Link>
-      </div>
+      {/* </div> */}
     </div>
   )
 }
