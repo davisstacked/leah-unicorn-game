249acf2b9c7a6d5dd41c83c6630ad6ba046c25ac
davisstacked <brettzvi@gmail.com>
2021-04-29 16:37:24 -0700

    completed form

---
+++ src/Game/Setup/Form.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const Form = ({ handleSubmit, handleChange, formData }) => {
+  return (
+    <div className="formContainer">
+      <form
+        className="form"
+        onSubmit={handleSubmit}
+      >
+        <h3 className="gameSettings">Game Settings</h3>
+        <div className="nameOptionsContainer">
+          <label htmlFor="name">
+            Name
+          </label>
+          <select
+            name="name"
+            id="name"
+            onChange={handleChange}>
+            <option value="Pinkie Pie">Pinkie Pie</option>
+            <option value="Twilight Sparkle">Twilight Sparkle</option>
+            <option value="Fluttershy">Fluttershy</option>
+            <option value="AppleJack">Applejack</option>
+            <option value="Rainbow Dash">Rainbow Dash</option>
+            <option value="Rarity">Rarity</option>
+          </select>
+        </div>
+        <div className="difficultyOptionsContainer">
+          <label htmlFor="size">
+            Difficulty
+          </label>
+          <input
+            type="range"
+            id="difficulty"
+            name="difficulty"
+            min={0}
+            max={10}
+            onChange={handleChange}
+            value={formData.difficulty}
+          />
+        </div>
+        <button className="startGameButton">
+          Start Game!
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/Game/Setup/SetupPage.js
@@ -0,0 +1,4 @@
+// import React, { useState, useContext } from 'react';
+// import GameContext from '../../Context/GameContext';
+// import Form from "./Form";
+
