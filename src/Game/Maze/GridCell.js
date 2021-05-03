b719935222b40451b6226673e0b36f1c2b8e7754
davisstacked <brettzvi@gmail.com>
2021-05-03 16:03:52 -0700

    completed GridCell.js

---
+++ src/Game/Maze/GridCell.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from "classnames";
+
+const GridCell = ({ borders, index, spritePositions }) => {
+  // conditionally renders border aroung cell based on borders prop
+  const gridCellClass = classNames({
+    "Grid-cell": true,
+    "border-east": borders.includes("east"),
+    "border-west": borders.includes("west"),
+    "border-north": borders.includes("north"),
+    "border-south": borders.includes("south"),
+  });
+
+  return (
+    <div className={gridCellClass}>
+      {index === spritePositions.end ? "🌈" : null}
+      {index === spritePositions.unicorn ? "🦄" : null}
+      {index === spritePositions.monster ? "💀" : null}
+    </div>
+  );
+};
+
+export default GridCell;
