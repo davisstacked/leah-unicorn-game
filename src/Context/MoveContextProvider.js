9c0d22f4eacf1275b35bca877155f24ac58792f7
davisstacked <brettzvi@gmail.com>
2021-05-03 16:50:24 -0700

    created handle move function in movecontext

--- src/Context/MoveContextProvider.js
+++ src/Context/MoveContextProvider.js
@@ -11,6 +11,29 @@ const MoveContextProvider = ({ children }) => {
   const { playLegal, playIllegal, playGameOver, playGameWon } = useContext(AudioContext);
   const [mazeId, setMazeId] = useState('');
 
-  
+  // sets focus on maze grid to enable hotkeys
+  useEffect(() => {
+    if (mazeId === '') return;
+    const Maze = document.querySelector("[tabindex='-1']");
+    Maze.focus();
+  }, [mazeId]);
 
+  // handles move
+  const handleMove = (direction) => {
+    const move = async (data) => {
+      try {
+        const res = await UnicornApi.makeMove(data, mazeId);
+        if (res["state-result"] === "Move accepted") {
+          playLegal();
+          updatePositions();
+        } else {
+          playIllegal();
+        }
+        checkForGameOver(res);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    move({ direction });
+  }
 }
