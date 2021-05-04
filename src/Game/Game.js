9ab3a73892dc4f0a708d3d8e40eda074c57400dc
davisstacked <brettzvi@gmail.com>
2021-05-04 16:39:40 -0700

    jsx for win page

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import GameContext from '../Context/GameContext';
 import SetupPage from "./Setup/SetupPage";
 import MazeWrapper from "./Maze/MazeWrapper";
+import WinPage from "./Win/WinPage";
 
 const Game = () => {
   const { gameData } = useContext(GameContext);
@@ -9,6 +10,7 @@ const Game = () => {
     <div>
       {gameData.status === "setup" && <SetupPage />}
       {gameData.status === "active" && <MazeWrapper />}
+      {gameData.status === "won" && <WinPage /> }
     </div>
   );
 };
---
+++ src/Game/Win/WinPage.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import GameContext from "../../Context/GameContext";
+import { useHistory } from "react-router-dom";
+import WinUnicornImage from "./WinUnicornImage";
+
+const WinPage = () => {
+  const { playAgain } = useContext(GameContext);
+  const history = useHistory();
+
+  const handlePlayAgain = () => {
+    playAgain();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <div className="flex flex-col justify-evenly items-center p-10 h-screen">
+        <div className="font-mono text-xl text-center">
+          You reached the rainbow!
+        </div>
+        <WinUnicornImage />
+        <button
+          className="hover:shadow-md hover:border-8 py-2 px-3 border-2 border-purple-700 rounded bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 font-mono"
+          onClick={handlePlayAgain}
+        >
+          Restart
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default WinPage;
