9fd8a902bf36e1f9ea8073a3ffdf5dc7f1dbb5d1
davisstacked <brettzvi@gmail.com>
2021-05-05 19:21:12 -0700

    more stylin

--- src/Game/Setup/SetupPage.js
+++ src/Game/Setup/SetupPage.js
@@ -26,22 +26,22 @@ const SetupPage = () => {
   };
 
   return (
-    <div className="pt-8 pb-8 text sm:h-screen bg-setup flex flex-col items-center text-center">
-      <h1 className="gradient-orchid border-2 border-black rounded font-mono px-2 justify-items-center items-center shadow-md mb-2">Game Setup</h1>
+    <div className="pt-8 pb-8 text sm:h-fill bg-setup flex flex-col items-center text-center">
+      <h1 className="gradient-orchid border-2 border-black rounded font-mono px-2 justify-items-center items-center shadow-md mb-2 text-pink-600 font-black">Game Setup</h1>
       <p className="gradient-pink text-white py-1 px-3 border-2 rounded">Help the <span className="text-blue-400">UNICORN</span> navigate the <span className="text-yellow-300">MAZE</span> towards the <span className="text-pink-300">RAINBOW</span>!
       </p>
       <div className="sm:mt-8 flex flex-col sm:grid sm:grid-flow-col sm:grid-cols-3">
         <SmallUnicorn />
         <div className="flex justify-center" >
           <img
-            className="h-36 sm:h-48 border-2 border-black"
+            className="h-36 sm:h-44 border-2 border-pink-400"
             src={Maze}
             alt="maze"
           />
         </div>
         <SmallRainbow />
       </div>
-        <p className="mt-8 text-2xl bg-black text-white py-1 px-3 border-2 border-black rounded">BEWARE OF THE <span className="text-red-600">SKULL MONSTER</span>! 
+        <p className="mt-6 text-2xl">BEWARE OF THE <span className="text-red-600 bg-black rounded px-1">SKULL MONSTER</span>!!!  
         </p>
       <div className="sm:w-3/6 sm:mt-8 flex-col flex sm:flex-row justify-evenly items-center mb-4">
         <SmallSkull/>
