365e794137356e0420a40bd7ee0ebed197948d9a
davisstacked <brettzvi@gmail.com>
2021-11-04 19:38:23 -0700

    added testing for lose page

---
+++ src/Game/Lose/LosePage.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import LosePage from "./LosePage";
+import GameContextProvider from "../../Context/GameContextProvider";
+import { MemoryRouter } from "react-router";
+
+test("renders without crashing", () => {
+  render(
+    <GameContextProvider>
+      <LosePage />
+    </GameContextProvider>
+  );
+});
+
+test("matches snapshot", () => {
+  const { asFragment } = render(
+    <GameContextProvider>
+      <LosePage />
+    </GameContextProvider>
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
